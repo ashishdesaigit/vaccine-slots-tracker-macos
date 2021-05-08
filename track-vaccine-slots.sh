10ab31a069f0395611adb9bc60c08a6075f3d02e
ashishdesai <ashishdesai21@gmail.com>
2021-05-08 20:24:42 +0530

    Correction

--- track-vaccine-slots.sh
+++ track-vaccine-slots.sh
@@ -4,10 +4,10 @@ bookingDate="08-05-2021"
 res="No Slots Available"
 notificationTitle="Vaccine Slot available - Slot available"
 while [[ "$res" == "$res" ]]; do
-    for pin in 416119 416005 416209 416216
+    for pin in 411027 411217 411038 792101
     do
         res=$(node index.js $pin $bookingDate)
-        if [ "$res" == "$res" ]; then
+        if [ "$res" != "No Slots Available" ]; then
         echo "for $pin, $res"
         osascript -e "display notification \"$res\" with title \"$notificationTitle\""
         fi
